4780c4580602bfba7fced74ab44674e493c044f8
Daniel Kuhn <dkuhn@pgh.yearup.org>
2022-10-18 15:43:37 -0400

    add rest of work from day

---
+++ conversion_tests.js
@@ -0,0 +1,51 @@
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+var a="  101.1"  ;
+var b="55";
+var c="402 Stevens"
+var d="Number 5";
+
+a=parseInt(a);
+b=parseInt(b);
+c=parseInt(c);
+d=parseInt(d);
+
+
+console.log(a,b,c,d);
+var a="  101.1"  ;
+var b="55";
+var c="402 Stevens"
+var d="Number 5";
+
+a=parseFloat(a);
+b=parseFloat(b);
+c=parseFloat(c);
+d=parseFloat(d);
+
+
+console.log(a,b,  c,  d);
+var a="  101.1"  ;
+var b="55";
+var c="402 Stevens"
+var d="Number 5";
+
+a=Number(a);
+b=Number(b);
+c=Number(c);
+d=Number(d);
+
+
+console.log(  a,  b,  c,  d);
+var a="  101.1"  ;
+var b="55";
+var c="402 Stevens"
+var d="Number 5";
+
+a="unary+a";
+b="unary+b";
+c="unary+c";
+d="unary+d";
+
+
+console.log(  a,  b,  c,  d);
---
+++ ct_table.md
@@ -0,0 +1,4 @@
+a, " 101.1 ",101,101.1,101.1,unary+a
+b, "55",55,55,55,unary+b
+c,"402 Stevens",402,402,Nan,unary+c
+d,"Number 5"NaN,NaN,NaN,unary+d
