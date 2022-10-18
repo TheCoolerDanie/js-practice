cd5e333c095b2e382892e567c65265c9c33659ac
Daniel Kuhn <dkuhn@pgh.yearup.org>
2022-10-18 11:59:05 -0400

    add taxes and tipamount

---
+++ taxes.js
@@ -0,0 +1,3 @@
+let fedTax=.23;
+let income=153;
+console.log((income * fedTax).toFixed(2))
--- tipamount.js
+++ tipamount.js
@@ -1,4 +1,4 @@
 let bill = 150;
 let tipPercent =0.15
 let tip = bill * tipPercent;
-console.log(tip)
+console.log(`The tip on $${bill.toFixed(2)} food bill is $${tip.toFixed(2)}`)
